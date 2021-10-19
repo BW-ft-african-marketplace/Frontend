0e93235ff14b7d2689997366206faa4ac2c0dc5c
Benedict Schurwanz <benedictify@gmail.com>
2021-10-19 10:48:03 -0500

    add Route for logout to App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Homepage from './components/HomePage/Homepage';
 import SignupPage from './components/SignUpPage/SignUpPage';
 import LogInPage from './components/LogInPage/LogInPage';
+import LogoutPage from './components/LogoutPage/LogoutPage';
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" component={LogInPage} />
         <Route path="/signup" component={SignupPage} />
         <Route exact path="/home" component={Homepage} />
+        <Route exact path="/logout" component={LogoutPage} />
         <Route path="/">
           <Redirect to="/home" />
         </Route>
