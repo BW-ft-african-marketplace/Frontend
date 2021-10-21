f3cb8493e1892210a4802f78ed778877b470d0d1
Benedict Schurwanz <benedictify@gmail.com>
2021-10-20 22:14:17 -0500

    create basic UserPage component, with route in App.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import './App.css';
 import Homepage from './components/HomePage/Homepage';
 import SignupPage from './components/SignUpPage/SignUpPage';
 import LogInPage from './components/LogInPage/LogInPage';
+import UserPage from './components/Users/UserPage';
 import Logout from './components/Logout/Logout';
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
-  
+
   return (
     <div className="App">
       <Switch>
-        <Route path="/login" component={LogInPage} />
-        <Route path="/signup" component={SignupPage} />
         <Route exact path="/home" component={Homepage} />
+        <Route path="/signup" component={SignupPage} />
+        <Route path="/login" component={LogInPage} />
+				<PrivateRoute exact path="/users/:id" component={UserPage} />
         <PrivateRoute exact path="/logout" component={Logout} />
         <Route path="/">
           <Redirect to="/home" />
